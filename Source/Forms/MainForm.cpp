f0e7c7c18a5e2192fa4d53d2693839d1569ce2a4
Viktor Popp <viktorpopp.dev@gmail.com>
2025-01-07 11:28:00 +0100

    Added DPI awareness using a manifest file

--- Source/Forms/MainForm.cpp
+++ Source/Forms/MainForm.cpp
@@ -1,10 +1,12 @@
 #include "MainForm.h"
+#include <Windows.h>
 
 using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThreadAttribute]
 int main(array<String^>^ args) {
+	SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	SystemUtils::MainForm form;
